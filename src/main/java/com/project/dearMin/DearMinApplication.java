eaf9125e969a0852280e4d68d649d893b09e6490
ez0603 <dlwldjs3132@naver.com>
2024-10-08 00:05:11 +0900

    Faat : 초기세팅

---
+++ src/main/java/com/project/dearMin/DearMinApplication.java
@@ -0,0 +1,13 @@
+package com.project.dearMin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearMinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearMinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dearMin/DearMinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dearMin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearMinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
